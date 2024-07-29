d2f4b8a6b9a72b7c36358e945ccc441eefc4e117
Sabeem101 <sabeemmouqere@gmail.com>
2024-07-29 05:57:17 -0700

    API data

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Returns information about a TODO list progress,
+for a given employee's ID using REST API.
+"""
+import requests
+import sys
+
+
+def main():
+    """
+    Returning employee's TODO list informations.
+    """
+    id = sys.argv[1]
+    url = f'https://jsonplaceholder.typicode.com/'
+    users = f'users?id={id}'
+    todos = f'todos?userId={id}'
+    done = f'{todos}&completed=true'
+    notDone = f'{todos}&completed=false'
+    Name = userData[0].get("name")
+    todosData = requests.get(f'{url}{todos}').json()
+    todosDone = requests.get(f'{url}{done}').json()
+    doneN = len(todosDone)
+    totalN = len(todosData)
+    print(f'Employee {Name} is done with tasks({doneN}/{totalN}):')
+    for task in todosDone:
+        print("\t "+task.get("title"))
+
+
+if __name__ == "__main__":
+    main()
